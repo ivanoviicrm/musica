c61a2cea5907ebb41d124cf364ddcebaae65fc7a
ivanoviicrm <47173713+ivanoviicrm@users.noreply.github.com>
2019-02-18 19:35:24 +0100

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', '10.128.181.221');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
 define('DB_DATABASE', 'musica');
